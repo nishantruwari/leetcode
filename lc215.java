eb088d134df9bf036ccebb9e7db96164da541644
nishantruwari <83151342+nishantruwari@users.noreply.github.com>
2024-09-09 11:54:22 +0530

    Create lc215.java

---
+++ lc215.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+        }
+
+        for(int i=1;i<k;i++){
+            pq.poll();
+        }
+        return pq.poll();
+
+
+        
+    }
+}
